50c13a7c6aac62647880b3a29c18bb9fd7a99b31
Wangzilin <wangzilinn@gmail.com>
2020-02-18 20:57:36 +0800

    写了LSTM, 解决了batch的问题, 应该可以用在单片机上

--- RNN_regressor.py
+++ RNN_regressor.py
@@ -1,6 +1,9 @@
 import numpy as np
+from keras import Sequential
+from keras.layers import LSTM, TimeDistributed, Dense
+from keras.optimizers import Adam
 
-np.random.seed(1996)
+# np.random.seed(1996)
 import matplotlib.pyplot as plt
 
 BATCH_START = 0
@@ -9,15 +12,48 @@ BATCH_SIZE = 50
 INPUT_SIZE = 1
 OUTPUT_SIZE = 1
 CELL_SIZE = 20
-LR = 0.006
+LR = 0.06
 
 
 def get_batch():
     global BATCH_START, TIME_STEPS
-    #
-    xs = np.arange(BATCH_START, BATCH_START + BATCH_SIZE * TIME_STEPS).reshape((BATCH_SIZE, TIME_STEPS)) / (10 * np.pi)  # BATCH_SIZE行, TIME_SREPS列
-    print(xs)
+    # arange(BATCH_START, BATCH_START + BATCH_SIZE * TIME_STEPS) 生成长度1000的等差数列
+    # reshape((BATCH_SIZE, TIME_STEPS)) 修改为BATCH_SIZE(50)行, 每行TIME_STEPS(20)个的矩阵
+    # 一行是一批, 一批有20个时间点, 一个方括号存多个数
+    xs = np.arange(BATCH_START, BATCH_START + BATCH_SIZE * TIME_STEPS).reshape((BATCH_SIZE, TIME_STEPS)) / (10 * np.pi)
+    seq = np.sin(xs)
+    res = np.cos(xs)
+    BATCH_START += TIME_STEPS
+    return [seq[:, :, np.newaxis], res[:, :, np.newaxis], xs]
 
-# for i in range(5):
-get_batch()
 
+model = Sequential()
+model.add(LSTM(
+    # batch_input_shape=(BATCH_SIZE, TIME_STEPS, INPUT_SIZE),
+    input_shape=(TIME_STEPS, INPUT_SIZE),
+    output_dim=CELL_SIZE,
+    return_sequences=True,  # output at all steps
+    # stateful=True,  # 每批之间保存状态
+))
+model.add(TimeDistributed(Dense(OUTPUT_SIZE)))
+adam = Adam(LR)
+model.compile(optimizer=adam, loss='mse')
+
+print("training...")
+X_batch, Y_batch, xs = get_batch()
+
+for step in range(501):
+    X_batch, Y_batch, xs = get_batch()
+    cost = model.train_on_batch(X_batch, Y_batch)
+    pred = model.predict(X_batch, BATCH_SIZE)
+    # print(X_batch.shape)
+    # print(pred.shape)
+    # plot:
+    plt.plot(xs[0, :], Y_batch[0].flatten(), 'r', xs[0, :], pred.flatten()[:TIME_STEPS], 'b--')
+    plt.ylim((-1.2, 1.2))
+    plt.draw()
+    plt.pause(0.01)
+    if step % 10 == 0:
+        print('train cost: ', cost)
+plt.show()
+model.save("h5/LSTM.h5")
--- test.py
+++ test.py
@@ -1,19 +1,43 @@
-from keras.datasets import mnist
-import matplotlib.pyplot as plt
-from keras.models import load_model
 import numpy as np
+from keras import Sequential
+from keras.engine.saving import load_model
+from keras.layers import LSTM, TimeDistributed, Dense
+from keras.optimizers import Adam
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-test = X_train[18]
-final = test[np.newaxis, :]
-model = load_model('h5/RNN_classifier.h5')
+# np.random.seed(1996)
+import matplotlib.pyplot as plt
 
-result = model.predict(X_train[:1])
+BATCH_START = 0
+TIME_STEPS = 20
+BATCH_SIZE = 50
+INPUT_SIZE = 1
+OUTPUT_SIZE = 1
+CELL_SIZE = 20
+LR = 0.006
 
-print(result)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-data = X_train[1]
-im = ax.imshow(data, cmap='viridis')
+def get_batch():
+    global BATCH_START, TIME_STEPS
+    # arange(BATCH_START, BATCH_START + BATCH_SIZE * TIME_STEPS) 生成长度1000的等差数列
+    # reshape((BATCH_SIZE, TIME_STEPS)) 修改为BATCH_SIZE(50)行, 每行TIME_STEPS(20)个的矩阵
+    # 一行是一批, 一批有20个时间点, 一个方括号存多个数
+    xs = np.arange(BATCH_START, BATCH_START + BATCH_SIZE * TIME_STEPS).reshape((BATCH_SIZE, TIME_STEPS)) / (10 * np.pi)
+    seq = np.sin(xs)
+    res = np.cos(xs)
+    BATCH_START += TIME_STEPS
+    return [seq[:, :, np.newaxis], res[:, :, np.newaxis], xs]
+
+
+model = load_model("h5/LSTM.h5")
+
+for step in range(1):
+    X_batch, Y_batch, xs = get_batch()
+    print(X_batch[:1, :, :].shape)
+    pred = model.predict(X_batch[:1, :, :])
+    print(pred.flatten())
+    plt.plot(xs[0, :], Y_batch[0].flatten(), 'r', xs[0, :], pred.flatten()[:TIME_STEPS], 'b--')
+    plt.ylim((-1.2, 1.2))
+    plt.draw()
+    plt.pause(0.01)
+
 plt.show()
